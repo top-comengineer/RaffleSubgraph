71ea3868bbf9821a8c7d40a6bcdf06aa6528a60f
top-comengineer <yunlong870924@gmail.com>
2024-02-13 09:25:32 +0300

    updated

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -1,18 +1,29 @@
 import {log} from '@graphprotocol/graph-ts';
 import {RaffleCreated} from '../types/RaffleFactory/RaffleFactory';
-import {RaffleFactory, Raffle} from '../types/schema';
+import {RaffleFactory, Raffle, RaffleLoader} from '../types/schema';
 import {FACTORY_ADDRESS, ADDRESS_ZERO} from './helpers';
 
 export function handleRaffleCreated(event: RaffleCreated): void {
-  let raffle = new Raffle(event.params.raffleContract.toHexString());
-  raffle.address = event.params.raffleContract;
+  console.log(`handleRaffleCreated, ${event.address.toString()}`)
   let raffleFactory = RaffleFactory.load(FACTORY_ADDRESS);
-  if (raffleFactory != null) {
-    let raffleLoader = raffleFactory.raffles; 
-    let raffles = raffleLoader.load(); 
-    raffles.push(raffle);
-    raffleFactory.save(); // Save the changes to the raffleFactory entity
-    raffle.raffleFactory = raffleFactory.id;
+  let raffle = new Raffle(event.params.raffleContract.toHexString());
+
+  if (raffleFactory == null) {
+    console.log("Created RaffleFactory")
+    raffleFactory = new RaffleFactory(FACTORY_ADDRESS);
+    // let raffleLoader =  raffleFactory.raffles;
+    // let raffles = raffleLoader.load();
+    // raffles.push(raffle);
+
+    // raffleFactory.save();
   }
-  raffle.save();
+
+  // raffle.id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
+  // raffle.address = event.params.raffleContract;
+  // raffle.save();
+
+  // let raffleLoader = raffleFactory.raffles;
+  // let raffles = raffleLoader.load();
+  // raffles.push(raffle)
+  // raffleFactory.save();
 }
