1df93d529c8756caa0433eb6598ffea995b136ea
top-comengineer <yunlong870924@gmail.com>
2024-02-20 20:06:40 +0100

    updated

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -1,10 +1,19 @@
-import { TicketSold } from '../types/templates/Raffle/Raffle'; // Update the path based on the actual location of the generated types
+import { TicketSold } from '../types/templates/Raffle/Raffle';
 import { Mint, Raffle } from '../types/schema';
 
 export function handleTicketSold(event: TicketSold): void {
-  let raffle = Raffle.load(event.address.toHexString()); 
-  if (raffle != null) {
-    let mint = new Mint(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+  let raffle = Raffle.load(event.address.toHexString());
+
+  if (!raffle) {
+    raffle = new Raffle(event.address.toHexString());
+    raffle.save();
+  }
+
+  let mintId = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  let mint = Mint.load(mintId);
+
+  if(!mint) {
+    mint = new Mint(mintId);
     mint.blockNumber = event.block.number;
     mint.timestamp = event.block.timestamp;
     mint.raffleId = raffle.id;
--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -1,18 +1,28 @@
 import {log} from '@graphprotocol/graph-ts';
 import {RaffleCreated} from '../types/RaffleFactory/RaffleFactory';
-import {RaffleFactory, Raffle} from '../types/schema';
+import {RaffleFactory, Raffle, RaffleLoader} from '../types/schema';
 import {FACTORY_ADDRESS, ADDRESS_ZERO} from './helpers';
+import {TicketSold} from '../types/templates/Raffle/Raffle'
 
 export function handleRaffleCreated(event: RaffleCreated): void {
-  let raffle = new Raffle(event.params.raffleContract.toHexString());
-  raffle.address = event.params.raffleContract;
   let raffleFactory = RaffleFactory.load(FACTORY_ADDRESS);
-  if (raffleFactory != null) {
-    let raffleLoader = raffleFactory.raffles; 
-    let raffles = raffleLoader.load(); 
+  let raffle = new Raffle(event.params.raffleContract.toHexString());
+
+  if (raffleFactory == null) {
+    raffleFactory = new RaffleFactory(FACTORY_ADDRESS);
+    let raffleLoader =  raffleFactory.raffles;
+    let raffles = raffleLoader.load();
     raffles.push(raffle);
-    raffleFactory.save(); // Save the changes to the raffleFactory entity
-    raffle.raffleFactory = raffleFactory.id;
+
+    raffleFactory.save();
   }
+
+  raffle.id = event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
+  raffle.address = event.params.raffleContract;
   raffle.save();
+
+  let raffleLoader = raffleFactory.raffles;
+  let raffles = raffleLoader.load();
+  raffles.push(raffle)
+  raffleFactory.save();
 }
